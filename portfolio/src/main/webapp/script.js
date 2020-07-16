a24351e35a684f138b2c6cb4d1d580d397fb80bd
Ioana Nedelcu <ioannanedelcu@gmail.com>
2020-07-16 14:31:55 +0000

    Add js feature (buttons that change grid view)

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,28 +1,27 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Get the pictures in the grid in pictures variable
+var pictures = document.getElementsByClassName("grid-column");
+var i; // contor
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+// One column images view
+function one() {
+  for (i = 0; i < pictures.length; i++) {
+    pictures[i].style.msFlex = "100%";
+    pictures[i].style.flex = "100%";
+  }
+}
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+// Two columns images view
+function two() {
+  for (i = 0; i < pictures.length; i++) {
+    pictures[i].style.msFlex = "50%";
+    pictures[i].style.flex = "50%";
+  }
+}
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+// Four columns images view
+function four() {
+  for (i = 0; i < pictures.length; i++) {
+    pictures[i].style.msFlex = "25%";
+    pictures[i].style.flex = "25%";
+  }
 }
